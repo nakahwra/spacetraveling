b36d400f84e4a57f98ba1b9e859c9d396be85ae2
Lucas Nakahara <nakahwra@gmail.com>
2021-08-24 13:25:48 -0300

    💄 Create and stylize home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 interface Post {
   uid?: string;
@@ -26,7 +29,44 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <h1>Hello spacetravelling</h1>
+    <>
+      <Head>
+        <title>Home - Spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time><FiCalendar className={styles.icons} /> 15 Mar 2021</time>
+              <span><FiUser className={styles.icons} /> Lucas Nakahara</span>
+            </div>
+          </a>
+
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time><FiCalendar className={styles.icons} /> 15 Mar 2021</time>
+              <span><FiUser className={styles.icons} /> Lucas Nakahara</span>
+            </div>
+          </a>
+
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time><FiCalendar className={styles.icons} /> 15 Mar 2021</time>
+              <span><FiUser className={styles.icons} /> Lucas Nakahara</span>
+            </div>
+          </a>
+
+          <span className={styles.loadPosts} >Carregar mais posts</span>
+        </div>
+      </main>
+    </>
   );
 }
 
