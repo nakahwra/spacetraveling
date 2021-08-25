657e02977105d943db46f66b649b1cdc9788e948
Lucas Nakahara <nakahwra@gmail.com>
2021-08-25 13:20:12 -0300

    ✨ Create helper date formatter

---
+++ src/utils/helpers.ts
@@ -0,0 +1,6 @@
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+export function dateFormat(date: string) {
+  return format(new Date(date), "dd MMM yyyy", { locale: ptBR })
+}
