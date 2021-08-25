79228d33009cefe104c91170034311ffb87301ce
Lucas Nakahara <nakahwra@gmail.com>
2021-08-25 14:18:37 -0300

    ✨ Add link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./header.module.scss";
+import Link from "next/link";
 
 export default function Header() {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="logo.png" alt="logo" />
+        <Link href="/">
+         <img src="logo.png" alt="logo" />
+        </Link>
       </div>
     </header>
   )
